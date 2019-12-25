f3fe07770c81101fa86db64db4e4a601d674539e
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-25 13:51:19 +0200

    Updates

---
+++ precache-manifest.003b4c2bbf4f66a72744b3c50ebb453f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2024955c6b1d8a4b72426aec00a5a8",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "06d8308faea0b69e77ef",
+    "url": "/react_tabs-with-router/static/css/main.108cf939.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "06d8308faea0b69e77ef",
+    "url": "/react_tabs-with-router/static/js/main.af7c9d04.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
